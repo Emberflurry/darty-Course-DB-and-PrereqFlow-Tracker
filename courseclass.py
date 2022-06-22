058e88414ad1392442cad3a88f079f3b3803e72b
John DeForest <juandelbosque373@gmail.com>
2022-06-22 01:14:25 -0400

    Add netTester, cleaning up courseclass
    actual networkX testing with my DB and classes, courseclass needed cleanup and some default fixings

--- courseclass.py
+++ courseclass.py
@@ -5,27 +5,29 @@
 
 
 class Course:
-    def __init__(self, dept, number, title, profs, pastterms, curfutterms, xlist, prereqs, prereqnotes, xtrafee,
-                 distribs, nroelig, typicalblocks, crn, coursetype, worldcults, assocLab, coursecredits):
+    def __init__(self, dept, coursenum, coursetitle="", profs="", pastterms="", curfutterms="", xlist="", prereqs="", prereqnotes="", xtrafee=False,
+                 distribs="", nroelig=True, typicalblocks="", crn="", coursetype="", worldcults="", assocLab=False, coursecredits=""):
         # ADD =[] to LISTS^ (when applicable lol)
 
         self.dept = str(dept)  # 4dig
-        self.number = str(number)  # 3dig or 1dig.2dig or 2dig.2dig
-        self.title = str(title)  # duh
+        self.number = str(coursenum)  # 3dig or 1dig.2dig or 2dig.2dig
+        
+        # not required:
+        self.title = str(coursetitle)  # duh
         self.profs = str(profs)    # eventually a list
         self.pastterms = str(pastterms)  # eventually a list
         self.curfutterms = str(curfutterms)  # eventually a list
         self.xlist = str(xlist)    # eventually a list
         self.prereqs = str(prereqs)  # eventually a list
         self.prereqnotes = str(prereqnotes)
-        self.xtrafee = xtrafee  # boolean
+        self.xtrafee = bool(xtrafee)  # boolean
         self.distribs = str(distribs)  # eventually a list?
-        self.isNROelig = nroelig  # boolean
+        self.isNROelig = bool(nroelig)  # boolean
         self.typicalblocks = str(typicalblocks)  # eventually a list
         self.CRN = str(crn)  # eventually an INT. course reg number? might b term specific...
         self.coursetype = str(coursetype)  # 3dig: lec vs lab
         self.worldcults = str(worldcults)  # world culture distribs, eventually a list
-        self.hasAssocLab = assocLab  # boolean
+        self.hasAssocLab = bool(assocLab)  # boolean
         self.credits = coursecredits  # int or float
 
 
---
+++ networkXtesting2.py
@@ -0,0 +1,5 @@
+import networkx as nx
+import plotly.graph_objects as go
+
+myG = nx.DiGraph()  # empty directed graph  https://networkx.org/documentation/stable/tutorial.html#directed-graphs
+myG.add_node()
