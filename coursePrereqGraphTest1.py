7c1fd8f2dc12beb9b23fd27eb86cb922bf4aeb5a
John DeForest <juandelbosque373@gmail.com>
2022-06-24 02:10:56 -0400

    can handle None/No prereqs i think

--- coursePrereqGraphTest1.py
+++ coursePrereqGraphTest1.py
@@ -13,7 +13,8 @@ hash2coursenameDict = {"0001": "CHEM040",
                        "0008": "CHEM040",
                        "0009": "PHYS031",
                        "0010": "PHYS016",
-                       "0011": "PHYS014"}
+                       "0011": "PHYS014",
+                       "0012": "CHEM005"}
 myG = nx.DiGraph()
 for i in hash2coursenameDict:
     myG.add_node(hash2coursenameDict[i])
@@ -22,38 +23,42 @@ for i in hash2coursenameDict:
 # plt.show()
 
 hash2prereqsDict = {"CHEM040": "MATH008/PHYS013:PHYS015:PHYS004/CHEM006:CHEM010",
-                    "PHYS031": "PHYS013/PHYS016/PHYS014:PHYS015"
+                    "PHYS031": "PHYS013/PHYS016/PHYS014:PHYS015",
+                    "CHEM006": "CHEM005"
                     }
 
-global orCounter
-orCounter=0
+global orCounter  # bc the OR nodes need to have unique IDs
+orCounter = 0
 def crsgraphcreator(courseHash):
     global orCounter
     courseName = hash2coursenameDict[courseHash]
     prereqString = hash2prereqsDict[courseName].strip()
-    allPrConditions = prereqString.split("/")
-    print(allPrConditions)
+    if prereqString != "" and prereqString != None:
+        allPrConditions = prereqString.split("/")
+        print(allPrConditions)
 
-    # orCounter = 0
-    for ea in allPrConditions:
-        if ":" in ea:  # OR List
-            orItems = ea.strip().split(":")
-            print(orItems)
+        for ea in allPrConditions:
+            if ":" in ea:  # OR List
+                orItems = ea.strip().split(":")
+                print(orItems)
 
-            newOrNode = str("or" + str(orCounter))
-            myG.add_node(newOrNode)
-            myG.add_edge(newOrNode, courseName)
+                newOrNode = str("or" + str(orCounter))
+                myG.add_node(newOrNode)
+                myG.add_edge(newOrNode, courseName)
 
-            for eaOrPrq in orItems:
-                myG.add_edge(eaOrPrq, newOrNode)
-            orCounter += 1
+                for eaOrPrq in orItems:
+                    myG.add_edge(eaOrPrq, newOrNode)
+                orCounter += 1
 
-        else:  # single course
-            myG.add_edge(ea, courseName)
+            else:  # single course
+                myG.add_edge(ea, courseName)
+    else:
+        print(courseName+" hasNoPrqs")
 
 
 crsgraphcreator("0008")
 crsgraphcreator("0009")
+crsgraphcreator("0002")
 
 nx.draw(myG, with_labels=True, font_weight='bold',node_color='grey')
 plt.show()
